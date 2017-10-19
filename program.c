7ad65df9c1a27179f844ab77c3a0d6820b6dfa50
Przemek <jezutekp@gmail.com>
2017-10-19 16:05:04 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,17 @@
+/*dolocz biblioteke standardowa input- autput*/
+#include <stdio.h>
+
+/* y=f(x)*/
+/*0=main() */
+
+
+
+int main()	/*funkcja glowna programu, zwraca wartosc calkowita*/
+
+{	/*miedzy klamrami jest cialo funkcji */
+	puts("Pierwszy program");	/* funkca z biblioteki studio*/
+
+	return 0;	/* zwraca wartosc zero*/
+
+}
+
